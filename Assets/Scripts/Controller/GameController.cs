46203a919c5a55ad3e63372c38b9b2507bc32a40
chaiwwei <caiweiwei@whu.edu.cn>
2024-04-15 12:26:39 +0800

    refine TaskScene: add players to the scene

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -8,9 +8,9 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private GameObject obj_BornPos, obj_PKPos, camera_Map;//玩家出生点，小地图摄像机
+    [SerializeField] private GameObject obj_BornPos, camera_Map;//玩家出生点，小地图摄像机
     [SerializeField] private GameObject obj_Avatar;//玩家不同角色预制体
-    [SerializeField] private string s_PkScene;
+
     private GameObject player;//玩家
 
     void Start()
@@ -18,7 +18,9 @@ public class GameController : MonoBehaviour
         //创建角色
         if (obj_Avatar != null)
         {
-            CreateAvatar(obj_Avatar);
+            player = Instantiate(obj_Avatar, obj_BornPos.transform.position, obj_BornPos.transform.rotation);
+
+            player.name = "Player";
         }
     }
 
@@ -28,24 +30,6 @@ public class GameController : MonoBehaviour
         camera_Map.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 15, player.transform.position.z);
     }
 
-    //创建并初始化玩家实体
-    void CreateAvatar(GameObject avatar)
-    {
-
-         player = Instantiate(avatar, obj_BornPos.transform.position, obj_BornPos.transform.rotation);
-
-         player.name = "Player";
-        /*if (GameInfo.GetPos() == ConstString.pos_Born)
-        {
-           
-            
-        }
-        else if (GameInfo.GetPos() == ConstString.pos_PK)
-        {
-            player = Instantiate(avatar, obj_PKPos.transform.position, obj_BornPos.transform.rotation);
-            player.name = "Player";
-        }*/
-    }
 
     //返回出生点
     public void ReturnBornPos()
@@ -55,10 +39,4 @@ public class GameController : MonoBehaviour
         player.GetComponent<AvatarController>().CanMove(true);
     }
 
-    //进入PK场景，并记录PK坐标点为下次玩家进入该场景的初始坐标
-    public void GoPKScene()
-    {
-        //SceneManager.LoadScene(s_PkScene);
-        GameInfo.SetPos(ConstString.pos_PK);
-    }
 }
